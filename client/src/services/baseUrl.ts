c9d05ec2c768c3b4e670b70a2ea22d3b7782711f
Muhammad-Bilal300 <bilalaarbi300@gmail.com>
2025-05-30 01:47:11 +0500

    change baseUrl in the client services

--- client/src/services/baseUrl.ts
+++ client/src/services/baseUrl.ts
@@ -7,11 +7,11 @@ const node_env = "production";
 const isProduction = node_env === "production";
 
 const baseUrl: string = isProduction
-  ? "https://sociofy-backend-f83x.onrender.com/api/v1" // relative path in production
+  ? "https://sociofy-backend-f24s.onrender.com/api/v1" // relative path in production
   : "http://localhost:8080/api/v1";
 
 export const imageBaseUrl: string = isProduction
-  ? "https://sociofy-backend-f83x.onrender.com"
+  ? "https://sociofy-backend-f24s.onrender.com"
   : "http://localhost:8080";
 
 export default baseUrl;
